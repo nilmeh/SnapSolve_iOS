0c4cd3a82c2bc267f391bcab690c242d4e7addd1
Kartik Bhatia <kartikbhatia2006@gmail.com>
2025-04-26 14:21:25 -0700

    backend setup done

--- SnapSolve/Backend/app.js
+++ SnapSolve/Backend/app.js
@@ -17,3 +17,15 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB!'))
+.catch((error) => console.error('MongoDB connection error:', error));
+
